9c960b42617b677f2fbfbd11e109311ee526a3d7
gt8mar <35577361+gt8mar@users.noreply.github.com>
2018-02-01 15:09:32 -0500

    Create Forst_WeightPlanets.py

---
+++ Forst_WeightPlanets.py
@@ -0,0 +1,10 @@
+# Marcus Forst
+# Weights on Different Planets
+
+
+# Inputs: planet name, altitude above avg radius, explorer mass
+
+f=open(planet_data.dat, r)
+pname=input.('Planet Name')
+alt=input.('Altitude above average radius')
+#if pname==[row, column?] then r=[row, column] and den=[row, column+1] 
