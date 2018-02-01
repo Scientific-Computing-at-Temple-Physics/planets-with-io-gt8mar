4674e6ca6611a725cd7eeff7a1a8782a6ecb6cc5
gt8mar <35577361+gt8mar@users.noreply.github.com>
2018-02-01 15:26:53 -0500

    Update Forst_WeightPlanets.py

--- Forst_WeightPlanets.py
+++ Forst_WeightPlanets.py
@@ -4,7 +4,9 @@
 
 # Inputs: planet name, altitude above avg radius, explorer mass
 
-f=open(planet_data.dat, r)
+f=open("planet_data.dat", "r")
+#for line in f,
+  
 pname=input.('Planet Name')
 alt=input.('Altitude above average radius')
 #if pname==[row, column?] then r=[row, column] and den=[row, column+1] 
